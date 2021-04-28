9693ffd8bc93b56ccf2edb831b1137605da366ab
kongsubin <qls0919@naver.com>
2021-04-28 16:14:34 +0900

    init

---
+++ src/main/java/handong/the/como/ThecomoApplication.java
@@ -0,0 +1,13 @@
+package handong.the.como;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handong/the/como/ThecomoApplicationTests.java
@@ -0,0 +1,13 @@
+package handong.the.como;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
