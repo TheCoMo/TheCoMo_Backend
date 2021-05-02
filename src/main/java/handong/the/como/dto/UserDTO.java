8eeeb1b2ea11d03cdcb6c626d03c25fbca86b9fc
newForinux <you_maybe@naver.com>
2021-05-02 21:36:17 +0900

    📚 Add User transfer object.

---
+++ src/main/java/handong/the/como/dto/UserDTO.java
@@ -0,0 +1,39 @@
+package handong.the.como.dto;
+
+import handong.the.como.domain.entity.User;
+import lombok.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserDTO {
+    private long id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String depart;
+
+    public User toEntity() {
+        User build = User.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .depart(depart)
+                .build();
+
+        return build;
+    }
+
+    @Builder
+    public UserDTO(long id, String email, String password, String nickname, String depart) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.depart = depart;
+    }
+}
